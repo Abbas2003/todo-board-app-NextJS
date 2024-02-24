2a9238ed6b918a99122c32073e5e86d7a8fdd235
Abbas2003 <abbas.mohammad805@gmail.com>
2024-02-24 12:43:27 +0500

    Canvas State Updated

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -7,61 +7,117 @@ import {
   StickyNote, 
   Type, 
   Undo2} from "lucide-react";
-import { ToolButton } from "./tool-button";
 
-export const Toolbar = () => {
+import { ToolButton } from "./tool-button";
+import { CanvasMode } from "@/types/canvas";
+import { CanvasState } from "@/types/canvas";
+import { LayerType } from "@/types/canvas";
+
+
+
+interface ToolbarProps {
+  canvasState: CanvasState
+  setCanvasState: (newState: CanvasState) => void
+  undo: () => void
+  redo: () => void
+  canUndo: boolean
+  canRedo: boolean
+}
+
+export const Toolbar = ({
+  undo,
+  redo,
+  canvasState,
+  setCanvasState,
+  canRedo,
+  canUndo,
+}: ToolbarProps) => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
         <ToolButton 
           label="Select"
           icon={MousePointer2}
-          onCLick={()=>{}}
-          isActive={false}
+          onCLick={()=> setCanvasState({ mode: CanvasMode.None, })}
+          isActive={
+            canvasState.mode === CanvasMode.None ||
+            canvasState.mode === CanvasMode.Translating ||
+            canvasState.mode === CanvasMode.SelectionNet ||
+            canvasState.mode === CanvasMode.Pressing ||
+            canvasState.mode === CanvasMode.Resizing 
+          }
         />
         <ToolButton 
           label="Text"
           icon={Type}
-          onCLick={()=>{}}
-          isActive={false}
+          onCLick={()=> setCanvasState({ 
+            mode: CanvasMode.Inserting,
+            layerType: LayerType.Text
+          })}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Text
+          }
         />
         <ToolButton 
           label="Sticky note"
           icon={StickyNote}
-          onCLick={()=>{}}
-          isActive={false}
+          onCLick={() => setCanvasState({
+            mode: CanvasMode.Inserting,
+            layerType: LayerType.Note
+          })}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Note
+          }
         />
         <ToolButton 
           label="Rectangle"
           icon={Square}
-          onCLick={()=>{}}
-          isActive={false}
+          onCLick={() => setCanvasState({
+            mode: CanvasMode.Inserting,
+            layerType: LayerType.Rectangle
+          })}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Rectangle
+          }
         />
         <ToolButton 
           label="Ellipse"
           icon={Circle}
-          onCLick={()=>{}}
-          isActive={false}
+          onCLick={() => setCanvasState({
+            mode: CanvasMode.Inserting,
+            layerType: LayerType.Ellipse
+          })}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting &&
+            canvasState.layerType === LayerType.Ellipse
+          }
         />
         <ToolButton 
           label="Pen"
           icon={Pencil}
-          onCLick={()=>{}}
-          isActive={false}
+          onCLick={() => setCanvasState({
+            mode: CanvasMode.Pencil,
+          })}
+          isActive={
+            canvasState.mode === CanvasMode.Pencil
+          }
         />
       </div>
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
         <ToolButton 
           label="Undo"
           icon={Undo2}
-          onCLick={()=>{}}
-          isDisabled={true}
+          onCLick={undo}
+          isDisabled={!canUndo}
         />
         <ToolButton 
           label="Redo"
           icon={Redo2}
-          onCLick={()=>{}}
-          isDisabled={true}
+          onCLick={redo}
+          isDisabled={!canRedo}
         />
       </div>
     </div>
